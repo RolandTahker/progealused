864d45aed42aebf656cfe35d1cc9bc904d26d740
RolandTahker <89915395+RolandTahker@users.noreply.github.com>
2021-09-17 12:28:49 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -1,1 +1,15 @@
+function checkTemp() {
+    let temp = Math.floor(Math.random() *60);
 
+    if (temp < 21) {
+        console.log ("liiga külm")
+    }
+
+    else if (temp < 20 && temp > 41) {
+        console.log ("temperatuur on paras")
+    }
+
+    else if (temp > 40) {
+        console.log ("liiga kuum")
+    }
+}
