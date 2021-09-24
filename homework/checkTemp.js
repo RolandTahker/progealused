35f9dd5e28744e3b853e4ebc4a42c0c6a83f621f
RolandTahker <89915395+RolandTahker@users.noreply.github.com>
2021-09-24 11:59:53 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -5,13 +5,13 @@ function checkTemp (temp) {
 //  let temp = 5;
 
 if (temp < 20) {
-    return(-1)
+    return -1
     } 
 if(temp >= 21 && temp <= 40) {
-    return(0)
+    return 0
     } 
 if (temp > 41) {
-    return(1)
+    return 1
     }
 }   
 
