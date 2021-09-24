ecc8c87646f7d35f5bd0fbe42f5a6dd304b47ff5
RolandTahker <89915395+RolandTahker@users.noreply.github.com>
2021-09-24 11:43:19 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -13,4 +13,20 @@ if(temp >= 21 && temp <= 40) {
 if (temp > 41) {
     return(1)
     }
+}   
+
+//  
+
+function checkTemp (temp) {
+    // let temp = 10;
+
+    if (temp < 21){
+        return("Temperatuur on liiga külm")
+    }
+    if (temp >= 21 && temp <= 40){
+        return("Temperatuur on soe")
+    }
+    if (temp > 40){
+        return("Temperatuur on kuum")
+    }
 }
