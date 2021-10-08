9e90b539631154cfe4153d83ac6be734e6b9e177
RolandTahker <89915395+RolandTahker@users.noreply.github.com>
2021-10-08 12:02:32 +0300

    Update prompt.js

--- homework/prompt.js
+++ homework/prompt.js
@@ -1,21 +1,26 @@
 password = prompt("Sisesta üks number 1-10-ni")
-let mituKordaProovinud = 0
+let proovis = 0
 let word = Math.floor(Math.random() * 10) + 1;
 while (password != word)
 {
-    mituKordaProovinud++
-    if (mituKordaProovinud == 1){
-    console.log("Oled proovinud " + mituKordaProovinud + " korra")
+   console.log(word)
+    proovis++
+    if (proovis == 1){
+    console.log("Oled proovinud " + proovis + " korra")
     }
-    else console.log("Oled proovinud " + mituKordaProovinud + " korda")
-    if (mituKordaProovinud == 3)
-        break
-
-
+    else console.log("Oled proovinud " + proovis + " korda")
+    if (proovis == 3){
+       break
+        }
+    if(password == word-1||password == word+1){
+    console.log("Ai, sa pakkusid väga lähedale.")
+}
     alert("Sorry, "+ password +" ei ole õige number.")
     password = prompt("Sisesta parool")
-    if (password == null)
+    if (password == null){
         break
-    if (password == word)
+        }
+    if (password == word){
 console.log("Yay! Arvasid õige numbri ära!")
+    }
 }
